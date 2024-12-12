17abbdf8e89dd052aba7093cfefeb71a5b9d878f
genn125 <genn125@gmail.com>
2024-12-12 10:26:27 +0300

    Способы вызова функции

--- module_3_2.py
+++ module_3_2.py
@@ -8,7 +8,7 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
 
     if ("@" and (".com" or ".ru" or ".net")) not in recipient and ("@" and (".com" or ".ru" or ".net")) not in sender:
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-
+        
     elif sender==recipient:
         print("Нельзя отправить письмо самому себе!")
 
