c8aa1b6534c00cd1cf1dbfb2778900e8caa7eca4
genn125 <genn125@gmail.com>
2024-12-16 14:17:32 +0300

    "Раз, два, три, четыре, пять .... Это не всё?":

--- module_3_6.py
+++ module_3_6.py
@@ -25,5 +25,4 @@ def calculate_structure_sum (data_structure):
 data_structure = [[1, 2, 3],{'a': 4, 'b': 5},(6, {'cube': 7, 'drum': 8}),
                   "Hello",((), [{(2, 'Urban', ('Urban2', 35))}])]
 
-result = calculate_structure_sum(data_structure)
-print(result)
+print(calculate_structure_sum(data_structure))
