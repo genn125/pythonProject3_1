e2ab87003b9b9766dd286df7f022b8a54a962c97
genn125 <genn125@gmail.com>
2024-12-16 19:31:01 +0300

    "А как делить?"

---
+++ fake_math.py
@@ -0,0 +1,11 @@
+
+def divide(first, second):
+
+    if second != 0:
+        a = first / second
+        return a
+    else:
+        return 'Ошибка'
+
+
+
---
+++ module_4_1.py
@@ -0,0 +1,14 @@
+
+# module_4_1
+
+#  "А как делить?"
+
+from fake_math import divide as dvd1
+from true_math import divide as dvd2
+
+
+
+print(dvd1(69, 3))
+print(dvd1(3, 0))
+print(dvd2(49, 7))
+print(dvd2(15, 0))
