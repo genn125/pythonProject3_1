ce1686e775dd5bba6eef00fd3ce015962c0b490a
genn125 <genn125@gmail.com>
2024-12-16 10:30:11 +0300

    Рекурсия - замена .replace(старый, новый, сколько)

--- module_3_4.py
+++ module_3_4.py
@@ -3,7 +3,7 @@
 
 # "Однокоренные" - использования параметров *args/ **kwargs на практике
 
-def single_root_words(txt, *other_words, root_words='гриб'):
+def single_root_words(*other_words, root_words='гриб'):
     same_words = []
 
     for i in other_words:
@@ -11,9 +11,9 @@ def single_root_words(txt, *other_words, root_words='гриб'):
         if root_words.lower() in i.lower() or i.lower() in root_words.lower():
             same_words.append(i)
 
-    return print(f"{txt} {', '.join([*same_words,])}", end='\n\n')   # Не понятно как распечатался список, который выводился кортежем
+    return print(same_words)
 
-single_root_words('"гриб" есть в словах:','грибник', 'гриБной', 'гробница', 'грибОЧЕк')
-single_root_words('В слове "обелиск" присутствуют слова:','Чёрный', 'Обе', 'двЕ', 'ЛИС',
+single_root_words('грибник', 'гриБной', 'гробница', 'грибОЧЕк')
+single_root_words('Чёрный', 'Обе', 'двЕ', 'ЛИС',
                   root_words = 'обелиск')
 
